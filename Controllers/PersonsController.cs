de5ce4ec917553669dfaa0210b4e601c4f2ed443
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-10-08 18:30:29 -0300

    Create the layer control for Person and the file content the code in View, the page in HTML

---
+++ Controllers/PersonsController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectInter.Controllers
+{
+   public class PersonsController : Controller
+   {
+       public ActionResult Index()
+       {
+           return View();
+       }
+       
+       [HttpGet]
+       public ActionResult Create()
+       {
+           return View();
+       }
+   } 
+}
--- Startup.cs
+++ Startup.cs
@@ -50,7 +50,7 @@ namespace ProjectInter
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Persons}/{action=Index}/{id?}");
             });
         }
     }
