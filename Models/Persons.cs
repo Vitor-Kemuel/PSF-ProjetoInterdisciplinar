7cad0791f88a386f0c30d5e54a686d340867365f
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-10-08 17:57:12 -0300

    Create class Person

---
+++ Models/Persons.cs
@@ -0,0 +1,22 @@
+namespace ProjectInter.Models
+{
+    public class Persons
+    {
+        
+        public int IdPerson { get; set; }
+
+        public string Name { get; set; }
+
+        public string Cellphone { get; set; }
+
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+    }
+}
+
+/*
+    git add . 
+    git commit -m "Create class Person"
+*/
