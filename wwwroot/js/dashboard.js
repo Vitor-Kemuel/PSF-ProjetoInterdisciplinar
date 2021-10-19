91c5d8f6c3363d635e95dafcd6b3cc7cdadf3303
Vitor-Kemuel <kemuvitor@gmail.com>
2021-10-19 15:24:46 -0300

    Adicionando botões de direção no painel de controle

---
+++ wwwroot/js/dashboard.js
@@ -0,0 +1,27 @@
+function cardWidth(size){
+    // var direction = document.querySelector('.cardOrderStatus');
+    // direction.style.width = size;
+    var divs =document.getElementsByTagName("div");
+    for(var i=0;i<=divs.length;i++) 
+    {
+        if(divs[i].className == "cardOrderStatus") 
+        {
+            divs[i].style.width = size;
+        }
+    }
+}
+
+
+function orderListDirection(){
+    var direction = document.querySelector('#orderTable');
+    direction.style.flexDirection = "column";
+    
+    cardWidth("auto")
+}
+
+function orderGridDirection(){
+    var direction = document.querySelector('#orderTable');
+    direction.style.flexDirection = "row";
+
+    cardWidth("300px")
+}
