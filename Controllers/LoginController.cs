a0d9efdc97bfeebdc9e3975e22587dde370291d6
Vitor-Kemuel <kemuvitor@gmail.com>
2021-10-18 21:09:35 -0300

    Alteração na pagina do painel de controle

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -32,8 +32,38 @@ namespace ProjectInter.Controllers
             order02.OrderAccepted = "15:17";
             order02.OrderDelivery = "15:21";
 
+            Order order03 = new Order();
+            order03.IdOrder = 3;
+            order03.Date = "18/10";
+            order03.Client = "Eduardo Junior";
+            order03.Situation = "Finalizado";
+            order03.OrderRead = "15:20";
+            order03.OrderAccepted = "15:20";
+            order03.OrderDelivery = "15:23";
+
+            Order order04 = new Order();
+            order04.IdOrder = 4;
+            order04.Date = "18/10";
+            order04.Client = "Fabricio Souza";
+            order04.Situation = "Em andamento";
+            order04.OrderRead = "15:30";
+            order04.OrderAccepted = "15:30";
+            order04.OrderDelivery = "--:--";
+
+            Order order05 = new Order();
+            order05.IdOrder = 5;
+            order05.Date = "18/10";
+            order05.Client = "Jessica Alves";
+            order05.Situation = "Aguardando aprovação";
+            order05.OrderRead = "15:32";
+            order05.OrderAccepted = "--:--";
+            order05.OrderDelivery = "--:--";
+
             orders.Add(order01);
             orders.Add(order02);
+            orders.Add(order03);
+            orders.Add(order04);
+            orders.Add(order05);
             
             return View(orders);
         }
