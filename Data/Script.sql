cf21bd958a01b211d20708c238af44cd6462f22f
Alex-San-15 <alex.receivement@gmai.com>
2021-11-05 18:05:57 -0300

    Alteracão das procedures do Banco de Dados

--- Data/Script.sql
+++ Data/Script.sql
@@ -108,9 +108,9 @@ tipo_produto      bit           not null,
 go
 
 
-/*
-use point_summer_foods_dev
-go
+--------------------------------------------------------------------------------------------
+-----------------------------PROCEDURE CADASTRO CLIENTES------------------------------------
+--------------------------------------------------------------------------------------------
 
 create procedure cadCliente(
 	@nome			 varchar(50),
@@ -118,101 +118,44 @@ create procedure cadCliente(
 	@email		     varchar(50),
 	@senha			 varchar(50),
 	@cpf			 varchar(14),
-	@estado			 varchar(40),
 	@endereco		 varchar(40),
 	@complemento	 varchar(100),
 	@numero_endereco varchar(10),
-	@cidade			 varchar(40),
 	@bairro			 varchar(40),
 	@cep			 varchar(10)
 )
 as 
 begin
 
-	insert into PESSOAS values(@nome,@celular,@email,@senha)
-	insert into CLIENTES values(@@identity,@cpf)
-	insert into LOGRADOURO values(@@identity,@estado, @endereco, @complemento, @numero_endereco, @cidade, @bairro, @cep)
+	declare @id as int
+
+	insert into PESSOAS values(@nome, @celular,@email,@senha)
+	set @id = @@identity
+	insert into CLIENTES values(@id, @cpf)
+	insert into LOGRADOUROS values(@id, @endereco, @complemento, @numero_endereco, @bairro, @cep)
 
 end
 go
 
-exec cadCliente 'Alex Gomes da Silva Filho', '17 988044110', 'alex.gsan11@gmail.com', '1234567', '46032190838', 'Sao Paulo', 'av.tarraf', 'sobrado', '3576', 'Mirassol', 'Portal Cidade Amiga', '15133296'
+exec cadCliente 'Guilherme Piovezan', '17 988840120', 'piovezan.guilherme@gmail.com', '1234567', '11111111111', 'av.São Paulo', 'mansão', '890', 'Centro', '15200000'
 
 go
-
-select * from CLIENTES, PESSOAS, LOGRADOURO
-
-go
-*/
 
 --------------------------------------------------------------------------------------------
------------------------------PROCEDURE CADASTRO CLIENTE-------------------------------------
+-----------------------------VIEW LISTA CLIENTES--------------------------------------------
 --------------------------------------------------------------------------------------------
 
-use point_summer_foods_dev
-go
-
-create procedure cadCliente(
-	@nome			 varchar(50),
-	@celular		 varchar(14),
-	@email		     varchar(50),
-	@senha			 varchar(50),
-	@cpf			 varchar(14)
-)
-as 
-begin
-
-	insert into PESSOAS values(@nome,@celular,@email,@senha)
-	insert into CLIENTES values(@@identity,@cpf)
-end
-go
-
-exec cadCliente 'Guilherme Piovezan', '17991380595', 'guilherme.piovezan@fatec.sp.gov.br', '1234567', '48502555820'
-go
-
-exec cadCliente 'Vitor Kemuel Carreiro Ponte', '017 98804-4345', 'kemuel.vitor@gmail.com', '1234567', '12345789905'
-go
-
-CREATE PROCEDURE cadLogradouro
-(
-	@id_clientes int,
-	@endereco    varchar(50),
-	@complemento varchar(50),
-	@numero      varchar(50),
-	@bairro      varchar(50),
-	@cep         varchar(50)
-)
-as 
-begin
-	insert into LOGRADOUROS VALUES (@id_clientes, @endereco, @complemento, @numero, @bairro, @cep)
-end
-
-EXEC cadLogradouro 5, 'Avenida São Paulo', 'Residencial', '890', 'Centro', '15200-000'
-
-exec cadLogradouro 9, 'Avenida São Paulo', 'Residencial', '890', 'Centro', '15200-000'
-
-
-select * from PESSOAS
-select * from CLIENTES
-select * from LOGRADOUROS
-
-
---------------------------------------------------------------------------------------------
------------------------------VIEW EXIBIÇÃO CADASTRO CLIENTE---------------------------------
---------------------------------------------------------------------------------------------
 
 create view v_listaClientes
 as
 
-	select p.nome, p.email, p.senha, p.celular, c.cpf, lg.endereco, lg.numero_endereco as numero, lg.bairro, lg.complemento, lg.cidade, lg.cep, lg.estado
+	select p.nome, p.email, p.senha, p.celular, c.cpf, lg.endereco, lg.numero_endereco as numero, lg.bairro, lg.complemento, lg.cep
 	from PESSOAS p, CLIENTES c, LOGRADOUROS lg
 	where p.id_pessoas = c.id_pessoas and lg.id_clientes = c.id_pessoas
 
 go
 
-select * from v_listaCliente
-
-
+select * from v_listaClientes
 --------------------------------------------------------------------------------------------
 -----------------------------PROCEDURE CADASTRO FUNCIONARIO---------------------------------
 --------------------------------------------------------------------------------------------
