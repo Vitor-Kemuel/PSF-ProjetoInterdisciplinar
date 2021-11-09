dc555d66e2967879d3c01f120995e880c3b45a15
Alex-San-15 <alex.receivement@gmai.com>
2021-11-09 18:16:05 -0300

    Atualizações de Tabelas  e procedures

--- Data/Script.sql
+++ Data/Script.sql
@@ -16,6 +16,13 @@ senha        varchar(50) not null
 
 go
 
+create table CLIENTES(
+id_pessoas  int         not null primary key  references PESSOAS,
+cpf         varchar(14) not null unique,
+)
+
+go
+
 create table FUNCIONARIOS(
 id_pessoas        int            not null primary key references PESSOAS,
 salario           decimal(10,2)  not null,
@@ -24,17 +31,10 @@ cargo             varchar(50)    not null,
 
 go
 
-create table CLIENTES(
-id_pessoas  int         not null primary key  references PESSOAS,
-cpf         varchar(14) not null unique,
-)
-
-go
-
 create table LOGRADOUROS(
 id_clientes      int          not null  primary key references CLIENTES,
 endereco         varchar(40)  not null,
-complemento      varchar(100) not null,
+complemento      varchar(100),
 numero_endereco  varchar(10)  not null,
 bairro           varchar(40)  not null,
 cep              varchar(10)  not null
@@ -49,6 +49,41 @@ situacao        bit            not null,-- 1=Ativo 2=Desativo
 nome            varchar(100)   not null,
 estoque         decimal(10,2)  not null,
 check (situacao in ( 1, 2))
+)
+
+go
+
+create table TIPO_PRODUTOS(
+id_tipo_produto   int           not null  primary key references PRODUTOS, 
+preco             decimal(10,2) not null,
+tipo_produto      bit           not null,
+)
+
+go
+
+create table PEDIDOS(
+id_pedidos        int            not null   primary key   identity,
+fk_clientes       int            not null   references    CLIENTES,
+cod_pedidos       varchar(40)    not null,
+quantidade        int            not null,
+observacoes       varchar(500),
+situacao          int            not null, -- 1=Leitura do pedido 2=Produzindo 3=Saiu pra entrega 
+valor_total       decimal(10,2)  not null,
+data_venda        datetime       not null,
+pedido_lido       datetime       not null, --Horario
+pedido_produzindo datetime       not null, --Horario
+pedido_entregue   datetime       not null, --horario
+check   (situacao in (1, 2, 3))
+)
+
+go
+
+create table PRODUTOS_PEDIDOS(
+id_produtos int           not null references PRODUTOS,
+id_pedidos  int           not null references PEDIDOS,
+quantidade  int           not null,
+valor_total decimal(7,2)  not null, 
+primary key (id_produtos,id_pedidos)              
 )
 
 go
@@ -69,48 +104,12 @@ primary key (id_produtos, id_compras)
 
 go
 
-create table PEDIDOS(
-id_pedidos        int            not null   primary key   identity,
-fk_clientes       int            not null   references  CLIENTES,
-fk_produtos       int            not null   references  PRODUTOS,
-cod_pedidos       varchar(40)    not null,
-quantidade        int            not null,
-observacoes       varchar(500),
-situacao          int            not null, -- 1=Leitura do pedido 2=Produzindo 3=Saiu pra entrega 
-valor_total       decimal(10,2)  not null,
-data_venda        datetime       not null,
-pedido_lido       datetime       not null, --Horario
-pedido_produzindo datetime       not null, --Horario
-pedido_entregue   datetime       not null  --horario
-check   (situacao in (1, 2, 3))
-)
 
+--------------------------------------PROCEDURES
+
+
+use point_summer_foods_dev
 go
-
-
-create table PRODUTOS_PEDIDOS(
-id_produtos int           not null references PRODUTOS,
-id_pedidos  int           not null references PEDIDOS,
-quantidade  int           not null,
-valor_total decimal(7,2)  not null, 
-primary key (id_produtos,id_pedidos)              
-)
-
-go
-
-
-create table TIPO_PRODUTOS(
-id_tipo_produto   int           not null  primary key references PRODUTOS, 
-preco             decimal(10,2) not null,
-tipo_produto      bit           not null,
-)
-
-go
-
-
---------------------------------------------------------------------------------------------
------------------------------PROCEDURE CADASTRO CLIENTES------------------------------------
---------------------------------------------------------------------------------------------
 
 create procedure cadCliente(
 	@nome			 varchar(50),
@@ -204,40 +203,30 @@ select * from v_listaFuncionario
 -----------------------------PROCEDURE CADASTRO PRODUTO-------------------------------------
 --------------------------------------------------------------------------------------------
 
+drop procedure cadProduto
+go
+
 create procedure cadProduto
 (
-	@cod_produto  varchar(40),
-	@situacao	  bit,-- 1=Ativo 2=Desativo
-	@nome		  varchar(100),
-	@estoque	  decimal(10,2)
-)
-as
-begin
-
-	insert into PRODUTOS values(@cod_produto, @situacao, @nome, @estoque)
-
-end
-
-create procedure cadTipoProduto
-(	
-	@id_tipo_produto int,
+	@cod_produto     varchar(40),
+	@situacao	     bit,-- 1=Ativo 2=Desativo
+	@nome		     varchar(100),
+	@estoque	     decimal(10,2),
 	@preco		     decimal(10,2),
 	@tipo_produto    bit           -- define se o produto é do tipo normal ou adicional
 )
 as
 begin
 
-	insert into TIPO_PRODUTOS values( @id_tipo_produto, @preco, @tipo_produto)
+	insert into PRODUTOS values(@cod_produto, @situacao, @nome, @estoque)
+	insert into TIPO_PRODUTOS values( @@IDENTITY, @preco, @tipo_produto)
+
 
 end
 
-
-
-exec cadProduto '1234', 1, 'açai premium', 22000.0
+exec cadProduto '1234', 1, 'açai premium', 22000.0, 122.00, 1  
 go
 
-exec cadTipoProduto  1 , 122.00, 1  
-go
 
 --------------------------------------------------------------------------------------------
 ----------------------------------VIEW EXIBIÇÃO CADASTRO PRODUTO----------------------------
@@ -253,7 +242,46 @@ go
 
 select * from v_listaProduto
 go
-
 --------------------------------------------------------------------------------------------
 -----------------------------PROCEDURE ALTERAR PRODUTO-------------------------------------
 --------------------------------------------------------------------------------------------
+
+create procedure altProduto
+(
+	@id_produtos     int,       
+	@cod_produto     varchar(40),
+	@situacao	     bit,          -- 1=Ativo 2=Desativo
+	@nome		     varchar(100),
+	@estoque         decimal(10,2),
+	@id_tipo_produto int,
+	@preco		     decimal(10,2),
+	@tipo_produto    bit           -- define se o produto é do tipo normal ou adicional
+)
+as
+begin
+	
+	update PRODUTOS set cod_produto = @cod_produto, situacao = @situacao, nome = @nome, estoque = @estoque
+	update TIPO_PRODUTOS set preco = @preco, tipo_produto = @tipo_produto
+
+end
+
+exec altProduto 2, '4321', 2, 'açai dourado', 19000.00, 119.00, 2
+go
+
+--------------------------------------------------------------------------------------------
+----------------------------------VIEW EXIBIÇÃO ALTERAR PRODUTO----------------------------
+--------------------------------------------------------------------------------------------
+
+
+create view v_listaAltProduto
+as
+	
+	select pro.cod_produto, pro.situacao, pro.nome, pro.estoque, tpro.preco, tpro.tipo_produto
+	from   PRODUTOS pro, TIPO_PRODUTOS tpro
+	where  pro.id_produtos = tpro.id_tipo_produto
+
+go
+
+select * from v_listaAltProduto
+
+go
