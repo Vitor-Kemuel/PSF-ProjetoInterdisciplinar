3e8959310690cac8b9d86c271557cff0fde2e6a7
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-11-14 10:10:39 -0300

    Realizando a utilização de alguns pacotes, ajustando a assinatura do método para receber uma instância de produtos e idem ao método update, no método de delete será utilizado o comando de DELETE e não UPDATE

--- Data/Repositories/ProductsRepository.cs
+++ Data/Repositories/ProductsRepository.cs
@@ -1,10 +1,14 @@
 using ProjectInter.Data.Interfaces;
+using System.Data.SqlClient;
+using System;
+using ProjectInter.Models;
+using System.Data;
 
 namespace ProjectInter.Data.Repositories
 {
     public class ProductsRepository : BDContext, IProductsRepository
     {
-        public void Create()
+        public void Create(Products products)
         {
             try
             {
@@ -14,11 +18,11 @@ namespace ProjectInter.Data.Repositories
                 cmd.CommandText = "cadProduto";
                 cmd.CommandType = CommandType.StoredProcedure;
 
-                cmd.Parameters.AddWithValue("@cod_produto", product.CodeProducts);
-                cmd.Parameters.AddWithValue("@nome", product.Name);
-                cmd.Parameters.AddWithValue("@estoque", product.Inventory);
-                cmd.Parameters.AddWithValue("@preco", product.Price);
-                cmd.Parameters.AddWithValue("@tipoproduto", product.TypeProduct);
+                cmd.Parameters.AddWithValue("@cod_produto", products.CodeProducts);
+                cmd.Parameters.AddWithValue("@nome", products.Name);
+                cmd.Parameters.AddWithValue("@estoque", products.Inventory);
+                //cmd.Parameters.AddWithValue("@preco", products.Price);
+                cmd.Parameters.AddWithValue("@tipoproduto", products.TypeProduct);
 
                 cmd.ExecuteNonQuery();
             }
@@ -49,7 +53,7 @@ namespace ProjectInter.Data.Repositories
             throw new System.NotImplementedException();
         }
 
-        public void UpdateProduct(int IdProduct)
+        public void UpdateProduct(Products products, int IdProduct)
         {
             try
             {
@@ -59,11 +63,11 @@ namespace ProjectInter.Data.Repositories
                 cmd.CommandText = "ProcedurePendente";
                 cmd.CommandType = CommandType.StoredProcedure;
 
-                cmd.Parameters.AddWithValue("@cod_produto", product.CodeProducts);
-                cmd.Parameters.AddWithValue("@nome", product.Name);
-                cmd.Parameters.AddWithValue("@estoque", product.Inventory);
-                cmd.Parameters.AddWithValue("@preco", product.Price);
-                cmd.Parameters.AddWithValue("@tipoproduto", product.TypeProduct);
+                cmd.Parameters.AddWithValue("@cod_produto", products.CodeProducts);
+                cmd.Parameters.AddWithValue("@nome", products.Name);
+                cmd.Parameters.AddWithValue("@estoque", products.Inventory);
+                //cmd.Parameters.AddWithValue("@preco", products.Price);
+                cmd.Parameters.AddWithValue("@tipoproduto", products.TypeProduct);
 
                 cmd.ExecuteNonQuery();
             }
@@ -83,7 +87,7 @@ namespace ProjectInter.Data.Repositories
                 cmd.Connection = connection;
                 cmd.CommandText = "UPDATE PRODUTOS set situacao = 1 where id_produtos = @id";
                 cmd.CommandType = CommandType.Text;
-                cmd.Parameters.AddWithValue("@id", id);
+                cmd.Parameters.AddWithValue("@id", IdProduct);
 
                 cmd.ExecuteNonQuery();
             }
@@ -93,6 +97,11 @@ namespace ProjectInter.Data.Repositories
             }finally{
                 Dispose();
             }
+        }
+
+        public void UpdateProdutc(Products products, int IdProduct)
+        {
+            throw new NotImplementedException();
         }
     }
 }
