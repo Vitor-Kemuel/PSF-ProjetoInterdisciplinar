d5105ba630a3357a8d8aafed7ec792edb7b5d9f0
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-11-18 21:38:48 -0300

    Alteração do tipo da propriedade Status em produto para int

--- Models/Products.cs
+++ Models/Products.cs
@@ -9,7 +9,7 @@ namespace ProjectInter.Models
         public IFormFile Image { get; set; }
         public string Name { get; set; }
         public float Inventory { get; set; }
-        public bool Status { get; set; } // 1 - Ativo 2 - Inativo
+        public int Status { get; set; } // 1 - Ativo 2 - Inativo
 
         #region Foreign Key
             public TypeProducts TypeProduct { get; set; }
