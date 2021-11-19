fba4bd55d83ac56db0262d2e28cf2c70992ccee2
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-11-19 18:00:03 -0300

    Adicionando uma nova propriedade ImageFile para atribuir o nome da imagem no banco

--- Models/Products.cs
+++ Models/Products.cs
@@ -6,7 +6,8 @@ namespace ProjectInter.Models
     public class Products
     {
         public int IdProducts { get; set; }
-        public IFormFile Image { get; set; }
+        public IFormFile ImageFile { get; set; }
+        public string Image { get; set; }
         public string Name { get; set; }
         public float Inventory { get; set; }
         public int Status { get; set; } // 1 - Ativo 2 - Inativo
