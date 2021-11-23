9bd7bc80e95ccb40ac680340c9dfbfe11671ec5e
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-11-23 17:28:57 -0300

    Criação do método create, falta apenas testa-lo

--- Data/Repositories/OrderRepository.cs
+++ Data/Repositories/OrderRepository.cs
@@ -2,28 +2,30 @@ using ProjectInter.Data.Interfaces;
 using ProjectInter.Models;
 using System.Data.SqlClient;
 using System.Data;
+using System;
 
 namespace ProjectInter.Data.Repositories
 {
     public class OrderRepository : BDContext, IdOrderRepository
     {
-        public void Create(Order order, ProductsOrders itens, int idCliente, int idFuncionario, int idProduto)
+        public void Create(Order order)
         {
             SqlCommand cmd = new SqlCommand();
             cmd.Connection = connection;
 
-            cmd.CommandText = "regVenda";
+            cmd.CommandText = "regVenda" + "SELECT @@IDENTITY";
             cmd.CommandType = CommandType.StoredProcedure;
 
-            cmd.Parameters.AddWithValue("@id_cliente", idCliente);
-            cmd.Parameters.AddWithValue("@id_funcionario", idFuncionario);
+            cmd.Parameters.AddWithValue("@id_cliente", order.Customer.IdPerson);
+            cmd.Parameters.AddWithValue("@id_funcionario", order.Employees.IdPerson);
             cmd.Parameters.AddWithValue("@observacoes", order.Observations);
             cmd.Parameters.AddWithValue("@situacao", order.Situation);
             cmd.Parameters.AddWithValue("@data_venda", order.DateToSell);
             cmd.Parameters.AddWithValue("@pedido_lido", order.OrderRead);
             cmd.Parameters.AddWithValue("@pedido_produzido", order.OrderAccepted);
             cmd.Parameters.AddWithValue("@pedido_entregue", order.OrderDelivery);
-            cmd.ExecuteNonQuery();
+
+            var idPedido = Convert.ToInt16(cmd.ExecuteScalar());
             
 
             foreach( var item in order.Itens){
@@ -31,7 +33,8 @@ namespace ProjectInter.Data.Repositories
                 SqlCommand cmdItem = new SqlCommand();
                 cmdItem.CommandText = "PRODUTO_PEDIDOS";
                 cmdItem.CommandType = CommandType.StoredProcedure;
-
+                
+                cmd.Parameters.AddWithValue("@id_pedido", item.IdPedido);
                 cmd.Parameters.AddWithValue("@id_produto", item.IdProduto);
                 cmd.Parameters.AddWithValue("@quantidade", item.Quantify);
                 cmd.Parameters.AddWithValue("@valor_total", item.ValueTotal); /* Naturalmente é calculado e não cadastrado */
@@ -47,7 +50,7 @@ namespace ProjectInter.Data.Repositories
             throw new System.NotImplementedException();
         }
 
-        public void GetOrderForSilgeCustomer(int idCustomer)
+        void IdOrderRepository.GetSingleOrder(int idOrder)
         {
             throw new System.NotImplementedException();
         }
