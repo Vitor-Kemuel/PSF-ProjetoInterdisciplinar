2695ba20a76b3d2881fb425ce1ed95d7d62e82ea
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-11-23 18:38:54 -0300

    Enolvendo código em try, catch e finally

--- Data/Repositories/OrderRepository.cs
+++ Data/Repositories/OrderRepository.cs
@@ -10,37 +10,46 @@ namespace ProjectInter.Data.Repositories
     {
         public void Create(Order order)
         {
-            SqlCommand cmd = new SqlCommand();
-            cmd.Connection = connection;
-
-            cmd.CommandText = "regVenda" + "SELECT @@IDENTITY";
-            cmd.CommandType = CommandType.StoredProcedure;
-
-            cmd.Parameters.AddWithValue("@id_cliente", order.Customer.IdPerson);
-            cmd.Parameters.AddWithValue("@id_funcionario", order.Employees.IdPerson);
-            cmd.Parameters.AddWithValue("@observacoes", order.Observations);
-            cmd.Parameters.AddWithValue("@situacao", order.Situation);
-            cmd.Parameters.AddWithValue("@data_venda", order.DateToSell);
-            cmd.Parameters.AddWithValue("@pedido_lido", order.OrderRead);
-            cmd.Parameters.AddWithValue("@pedido_produzido", order.OrderAccepted);
-            cmd.Parameters.AddWithValue("@pedido_entregue", order.OrderDelivery);
-
-            var idPedido = Convert.ToInt16(cmd.ExecuteScalar());
             
+            try
+            {
+                SqlCommand cmd = new SqlCommand();
+                cmd.Connection = connection;
 
-            foreach( var item in order.Itens){
+                cmd.CommandText = "regVenda" + "SELECT @@IDENTITY";
+                cmd.CommandType = CommandType.StoredProcedure;
 
-                SqlCommand cmdItem = new SqlCommand();
-                cmdItem.CommandText = "PRODUTO_PEDIDOS";
-                cmdItem.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@id_cliente", order.Customer.IdPerson);
+                cmd.Parameters.AddWithValue("@id_funcionario", order.Employees.IdPerson);
+                cmd.Parameters.AddWithValue("@observacoes", order.Observations);
+                cmd.Parameters.AddWithValue("@situacao", order.Situation);
+                cmd.Parameters.AddWithValue("@data_venda", order.DateToSell);
+                cmd.Parameters.AddWithValue("@pedido_lido", order.OrderRead);
+                cmd.Parameters.AddWithValue("@pedido_produzido", order.OrderAccepted);
+                cmd.Parameters.AddWithValue("@pedido_entregue", order.OrderDelivery);
+
+                var idPedido = Convert.ToInt16(cmd.ExecuteScalar());
                 
-                cmd.Parameters.AddWithValue("@id_pedido", item.IdPedido);
-                cmd.Parameters.AddWithValue("@id_produto", item.IdProduto);
-                cmd.Parameters.AddWithValue("@quantidade", item.Quantify);
-                cmd.Parameters.AddWithValue("@valor_total", item.ValueTotal); /* Naturalmente é calculado e não cadastrado */
+
+                foreach( var item in order.Itens){
+
+                    SqlCommand cmdItem = new SqlCommand();
+                    cmdItem.CommandText = "PRODUTO_PEDIDOS";
+                    cmdItem.CommandType = CommandType.StoredProcedure;
+                    
+                    cmd.Parameters.AddWithValue("@id_pedido", item.IdPedido);
+                    cmd.Parameters.AddWithValue("@id_produto", item.IdProduto);
+                    cmd.Parameters.AddWithValue("@quantidade", item.Quantify);
+                    cmd.Parameters.AddWithValue("@valor_total", item.ValueTotal); /* Naturalmente é calculado e não cadastrado */
 
 
-                cmdItem.ExecuteNonQuery();
+                    cmdItem.ExecuteNonQuery();
+                }
+                
+            } catch (Exception ex) {
+                throw new Exception(ex.Message);
+            } finally {
+                Dispose();
             }
 
         }
