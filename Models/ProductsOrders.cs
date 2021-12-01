6712b2d707ce78c1c78807501f1ed7020b9af26a
Guilherme Piovezan <guilherme.piovezan00@gmail.com>
2021-12-01 00:31:30 -0300

    Alterando o modelo de dados da entidade associativa entre produtos e pedidos

--- Models/ProductsOrders.cs
+++ Models/ProductsOrders.cs
@@ -3,13 +3,12 @@ namespace ProjectInter.Models
     public class ProductsOrders
     {
         public int IdPedido { get; set; }
-        public int IdProduto { get; set; }
+        public Products Produtos { get; set; }
         public int Quantify { get; set; }
         public double ValueTotal => Products.TypeProduct.Price * this.Quantify;
 
         #region Foreign Key
             public Products Products { get; set; }
-            public Order Order { get; set; }
         #endregion
 
     }
