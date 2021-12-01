6ce69bcce72cd1158d7e2ff91db604279a365c96
Vitor-Kemuel <kemuvitor@gmail.com>
2021-11-30 22:16:27 -0300

    adicionando js para a pagina de carrinho

---
+++ wwwroot/js/dashboard/cart.js
@@ -0,0 +1,9 @@
+function getSession(){
+    var getSession =              "<div id='sessionLeft'>";
+    var getSession = getSession + "<h2 class='subTitle'>Carrinho</h2>";
+    var getSession = getSession + "</div>";
+    var getSession = getSession + "<a id='OptionSession' href='#'>Finalizar compra</a>";
+
+    var session = document.getElementById("session");
+    session.innerHTML = session.innerHTML + getSession;
+}
