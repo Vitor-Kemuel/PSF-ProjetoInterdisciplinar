db41f66a4516b22b2fb15554d455e4a8930a8c9a
Vitor-Kemuel <kemuvitor@gmail.com>
2021-11-30 23:31:08 -0300

    Adicionando ancora para selecionar o cliente que realizou o pedido

--- wwwroot/js/dashboard/cart.js
+++ wwwroot/js/dashboard/cart.js
@@ -2,7 +2,7 @@ function getSession(){
     var getSession =              "<div id='sessionLeft'>";
     var getSession = getSession + "<h2 class='subTitle'>Carrinho</h2>";
     var getSession = getSession + "</div>";
-    var getSession = getSession + "<a id='OptionSession' href='#'>Finalizar compra</a>";
+    var getSession = getSession + "<a id='OptionSession' href='../DashboardCustomer/CartCustomer'>Finalizar compra</a>";
 
     var session = document.getElementById("session");
     session.innerHTML = session.innerHTML + getSession;
