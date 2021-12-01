ec10a40e44800964aed7308d90a3b8d1842e262c
Vitor-Kemuel <kemuvitor@gmail.com>
2021-11-30 22:15:34 -0300

    adicionando ancora para pagina do carrinho

--- wwwroot/js/dashboard/newOrder.js
+++ wwwroot/js/dashboard/newOrder.js
@@ -1,7 +1,8 @@
 function getSession(){
-    var getSession =              "<div id='sessionLeft'>";
+    var getSession =          "<div id='sessionLeft'>";
     getSession = getSession + "<h2 class='subTitle'>Novo pedido</h2>";
     getSession = getSession + "</div>";
+    getSession = getSession + "<a id='OptionSession' href='./cart'>Carrinho</a>";
 
     var session = document.getElementById("session");
     session.innerHTML = session.innerHTML + getSession;
